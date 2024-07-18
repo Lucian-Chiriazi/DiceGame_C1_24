00482b1b263e78159964bf3f788e245562e37c5f
Lucian Chiriazi <153869152+Lucian-Chiriazi@users.noreply.github.com>
2024-07-18 06:29:50 +0100

    Add Player getters and setters

--- src/dicegame/business/Player.java
+++ src/dicegame/business/Player.java
@@ -14,5 +14,49 @@ public class Player {
         this.playedTurn = false;
     }
 
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public String[] getPlayerScores() {
+        return playerScores;
+    }
+
+    public int getDiceLeft() {
+        return diceLeft;
+    }
+
+    public boolean getPlayedTurn() {
+        return playedTurn;
+    }
+
+    public String getTotalScore () {
+        int totalScore = 0;
+        for(String score : this.playerScores) {
+            if (score == null) {
+                totalScore += 0;
+            }else {
+                totalScore += Integer.parseInt(score);
+            }
+        }
+        return Integer.toString(totalScore);
+    }
+
+    public void setDiceLeft(int diceKept) {
+        this.diceLeft -= diceKept;
+    }
+
+    public void setPlayedTurn(boolean playedTurn) {
+        this.playedTurn = playedTurn;
+    }
+
+    public void setPlayerScores(int category, int score) {
+        this.playerScores[category] = Integer.toString(score);
+    }
+
+    public void resetVariables() {
+        this.diceLeft = 5;
+        this.playedTurn = false;
+    }
 
 }
