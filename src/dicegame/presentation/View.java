b3339acbd2345ff61a774ba13c89045614a22dce
Lucian Chiriazi <153869152+Lucian-Chiriazi@users.noreply.github.com>
2024-07-18 06:38:44 +0100

    Add startUI method to View class

--- src/dicegame/presentation/View.java
+++ src/dicegame/presentation/View.java
@@ -1,6 +1,7 @@
 package dicegame.presentation;
 
 import dicegame.business.Coordinator;
+import dicegame.business.Validation;
 
 import java.util.Scanner;
 
@@ -10,5 +11,22 @@ public class View {
 
     public View (Coordinator coordinator) {
         this.coordinator = coordinator;
+    }
+
+    public void startUI() {
+        System.out.println("Strategic dice game");
+        System.out.print("Play game (1) or Exit game (0) > ");
+        String input = scanner.nextLine().trim();
+
+        while(!Validation.startGameValidation(input)){
+            System.out.println("Invalid input");
+            System.out.print("Play game (1) or Exit game (0) > ");
+            input = scanner.nextLine().trim();
+        }
+        if ("0".equals(input)) {
+            System.out.println("Exit game!");
+        }else {
+            coordinator.startGameLogic();
+        }
     }
 }
